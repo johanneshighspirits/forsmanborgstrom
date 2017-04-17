dcec84b43a93ec6afbf18175a32ca72ffe25547d
Johannes Borgström <johannes@highspirits.se>
2017-04-17 12:26:48 +0200

    Add site layout test

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root "application#index"
-  get '/home',    to: 'projects/home'
-  get '/about',   to: 'projects/about'
-  get '/help',     to: 'projects/help'
+  root 'application#index'
+  get '/home',    to: 'projects#home'
+  get '/about',   to: 'projects#about'
+  get '/help',     to: 'projects#help'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
