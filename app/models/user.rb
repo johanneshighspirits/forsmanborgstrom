cf3d587a3089aa9640e016d7eb11b06ebbb2bb82
Johannes Borgström <johannes@highspirits.se>
2017-04-18 21:10:39 +0200

    Implement basic login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,12 @@ class User < ApplicationRecord
   validates :password, presence: true, length: { minimum: 6 }
   
   has_secure_password
+
+  # Return the hash digest of a given string
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+  
 end
--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -4,7 +4,7 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   test "layout links" do
     get home_path
     assert_template 'projects/home'
-    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", root_path, count: 3
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", about_path
     get about_path
