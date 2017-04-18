45d75b27ee0431254102f7b65ed95c89b521b2e6
Johannes Borgström <johannes@highspirits.se>
2017-04-18 14:18:42 +0200

    Finish User signup

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   get '/about',   to: 'projects#about'
   get '/help',    to: 'projects#help'
   get '/signup',  to: 'users#new'
+  post '/signup', to: 'users#create'
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 end
---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,42 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "should not add invalid user" do
+    get signup_path
+    assert_select 'form[action="/signup"]'
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user: {
+          name: "",
+          email: "user@invalid",
+          password: "foo",
+          password: "bar"
+        }
+      }
+    end
+    assert_template 'users/new'
+    # Check that error messages are shown
+    assert_select 'div#error_explanation'
+    assert_select 'div.alert.alert-danger'
+  end
+
+  test "should add valid user" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user: {
+          name: "Example User",
+          email: "user@example.com",
+          password: "foobarsecret",
+          password: "foobarsecret"
+        }
+      }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
+
+end
