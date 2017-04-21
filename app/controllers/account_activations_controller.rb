6c25137439cc9e12c78a764af86bb9d609915489
Johannes Borgström <johannes@highspirits.se>
2017-04-21 12:54:41 +0200

    Add account activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,2 +1,19 @@
 class AccountActivationsController < ApplicationController
+
+  # Called when user follows activation link from email, e.g.
+  # <host>/account_activations/<token>/edit?email=<email address>
+  # Where <token> is accessible through params[:id]
+  def edit
+    user = User.find_by(email: params[:email])
+    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+      user.activate
+      log_in user
+      flash[:success] = "Account activated!"
+      redirect_to user
+    else
+      flash[:danger] = "Invalid activation link"
+      redirect_to root_url
+    end
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -25,6 +25,16 @@ class User < ApplicationRecord
     SecureRandom.urlsafe_base64
   end
 
+  # Activate an account
+  def activate
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  # Sends activation email
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   # Remember user
   def remember
     # Generate random token
@@ -34,9 +44,10 @@ class User < ApplicationRecord
   end
 
   # Returns true if the given token matches the digest
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   # Forget user
